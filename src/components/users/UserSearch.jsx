735657cd1dc9907b4a227646672cce48cec05a15
harsh panchal <harshpanchal.silverwing@gmail.com>
2022-04-24 02:38:21 -0400

    user search component designed

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,53 @@
+import { React, useState, useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
+
+function UserSearch() {
+  const { user } = useContext(GithubContext);
+  const [text, setText] = useState('');
+
+  const handleChange = (param) => {
+    setText(param.target.value);
+  };
+
+  const handleSubmit = (param) => {
+    param.preventDefault();
+    if (text === '') {
+      alert('please enter something');
+    } else {
+      // @todo - search
+      setText('');
+    }
+  };
+  return (
+    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 mb-8'>
+      <div className=''>
+        <form action='' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <div className='relative'>
+              <input
+                value={text}
+                onChange={handleChange}
+                type='text'
+                className='w-full pr-40 bg-gray-200 input input-lg text-black'
+                placeholder='Search'
+              />
+              <button
+                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+                type='submit'
+              >
+                GO
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {user.length > 0 && (
+        <div className=''>
+          <button className='btn btn-ghost btn-lg'>clear</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserSearch;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { FaHome } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 function Home() {
   return (
     <div className=''>
+      <UserSearch />
       <UserResults />
     </div>
   );
